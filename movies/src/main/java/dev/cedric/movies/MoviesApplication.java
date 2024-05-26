22f3d0b4ac5d2061f937ff23a099e83583e4e504
cedricng <ngoupandecedric@hotmail.fr>
2024-05-26 16:37:42 +0200

    first commit

---
+++ movies/src/main/java/dev/cedric/movies/MoviesApplication.java
@@ -0,0 +1,21 @@
+package dev.cedric.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+
+
+}
